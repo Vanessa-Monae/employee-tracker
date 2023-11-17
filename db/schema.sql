2b4d7bb4b0e1e67dcf1a7a1e02d682a76e341e5c
vanessa moultrie <imvanessa16@gmail.com>
2023-11-17 10:50:46 -0500

    schema for department and ro table are complete

--- db/schema.sql
+++ db/schema.sql
@@ -1,18 +1,19 @@
-DROP DATABASE IF EXISTS movies_db;
-CREATE DATABASE movies_db;
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
 
-USE movies_db;
+USE employee_db;
 
-CREATE TABLE movies (
+CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  movie_name VARCHAR(100) NOT NULL
+  name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE reviews (
+CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    movie_id INT,
-    review TEXT NOT NULL,
-    FOREIGN KEY (movie_id)
-    REFERENCES movies(id)
-    ON DELETE SET NULL
+    department_id INT,
+    salary DECIMAL (10,2),
+    titles VARCHAR (30) NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE CASCADE
 );
